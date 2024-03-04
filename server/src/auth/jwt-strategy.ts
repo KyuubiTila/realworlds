90742c0f0ae231062d4de8b20082997c89caf4d5
KyuubiTila <tylerkatsumoto@gmail.com>
2024-03-04 19:09:15 +0000

    secret

--- server/src/auth/jwt-strategy.ts
+++ server/src/auth/jwt-strategy.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
-import * as config from 'config';
-
-const dbConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.SECRET || dbConfig.secret,
+      secretOrKey: process.env.SECRET || 'topsecret51',
     });
   }
 
